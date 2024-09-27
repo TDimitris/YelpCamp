3b3115bce524d7a1af1ed7405ef300dfc20d2cf4
Dimitris Tsioumas <95169128+TDimitris@users.noreply.github.com>
2024-09-27 20:12:21 +0300

    Update clusterMap.js

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -3,7 +3,7 @@ maptilersdk.config.apiKey = maptilerApiKey;
 const map = new maptilersdk.Map({
     container: 'cluster-map',
     style: maptilersdk.MapStyle.BRIGHT,
-    center: [-103.59179687498357, 40.66995747013945],
+    center: [23.701321, 39.114286],
     zoom: 3
 });
 
