b33d5b1fe3984ba35882f6a15be64686bf2fb361
Dimitris Tsioumas <95169128+TDimitris@users.noreply.github.com>
2024-09-27 20:22:33 +0300

    Update clusterMap.js

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -66,9 +66,9 @@ map.on('load', function () {
         filter: ['!', ['has', 'point_count']],
         paint: {
             'circle-color': '#11b4da',
-            'circle-radius': 4,
-            'circle-stroke-width': 1,
-            'circle-stroke-color': '#fff'
+            'circle-radius': 8,
+            'circle-stroke-width': 2,
+            'circle-stroke-color': '#0303fc'
         }
     });
 
